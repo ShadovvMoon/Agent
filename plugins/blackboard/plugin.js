5d39fda8faf55f84f9c1822b40e4fe98807531b4
Samuel Colbran <contact@samuco.net>
2015-01-09 16:09:51 +1000

    formatting improvements

--- plugins/blackboard/plugin.js
+++ plugins/blackboard/plugin.js
@@ -24,90 +24,90 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-var http 	 = require("http");
+var http     = require("http");
 var request  = require('request')
-var crypto 	 = require('crypto')
+var crypto   = require('crypto')
 var express  = require('express');
-var path 	 = require('path');
-var fs 		 = require('fs');
-var utils 	 = require('./oauth/lib/utils.js');
+var path     = require('path');
+var fs       = require('fs');
+var utils    = require('./oauth/lib/utils.js');
 var passport = require("passport");
 
 (function () {
     var root = module.exports;
-	function setupPlugin(core, settings) {
-		var app = core.app;
-		var plugin_port = app.get('port');
+    function setupPlugin(core, settings) {
+        var app = core.app;
+        var plugin_port = app.get('port');
 
-		//OAuth plugin
-		ConsumerStrategy = require('./oauth').Strategy;
-		
-		//Passport
-		passport.serializeUser(function(user, done) {
-		  	done(null, user);
-		});
-		passport.deserializeUser(function(obj, done) {
-		  	done(null, obj);
-		});
-		
-		var appList = {};
-		appList[settings.consumer_key] = {
-			secret: settings.shared_secret
-		}
-		
-		function findApp(key, next) {
-		    var consumer = appList[key];
-		    if (consumer) {
-		        next(null, {secret: consumer.secret});
-		   	} else {
-		        next(true);
-			}
-		}
-		
-		passport.use(new ConsumerStrategy(
-		  function(consumerKey, done) {
-			findApp(consumerKey, function(err, consumer) {
-			    if (err) {
-					return done(err);
-				}
-			    if (!consumer) {
-					return done(null, false);
-				}
-		        return done(null, consumer, consumer.secret);
-		    });
-		  },
-		  function checkTimestampAndNonce(timestamp, nonce, app, req, done) {
-		    var timeDelta = Math.round((new Date()).getTime() / 1000) - timestamp;
-		    if (timeDelta >= 10) {
-		        done(null, false);
-		    } else {
-		        done(null, true);
-			}
-		  }
-		));
+        //OAuth plugin
+        ConsumerStrategy = require('./oauth').Strategy;
+        
+        //Passport
+        passport.serializeUser(function(user, done) {
+              done(null, user);
+        });
+        passport.deserializeUser(function(obj, done) {
+              done(null, obj);
+        });
+        
+        var appList = {};
+        appList[settings.consumer_key] = {
+            secret: settings.shared_secret
+        }
+        
+        function findApp(key, next) {
+            var consumer = appList[key];
+            if (consumer) {
+                next(null, {secret: consumer.secret});
+            } else {
+                next(true);
+            }
+        }
+        
+        passport.use(new ConsumerStrategy(
+          function(consumerKey, done) {
+            findApp(consumerKey, function(err, consumer) {
+                if (err) {
+                    return done(err);
+                }
+                if (!consumer) {
+                    return done(null, false);
+                }
+                return done(null, consumer, consumer.secret);
+            });
+          },
+          function checkTimestampAndNonce(timestamp, nonce, app, req, done) {
+            var timeDelta = Math.round((new Date()).getTime() / 1000) - timestamp;
+            if (timeDelta >= 10) {
+                done(null, false);
+            } else {
+                done(null, true);
+            }
+          }
+        ));
 
-		//Create the access token url
-		fs.readFile('plugins/blackboard/index.html',function (err, html_data) {
-			if (err) {
-				console.log(err);
-			}
+        //Create the access token url
+        fs.readFile('plugins/blackboard/index.html',function (err, html_data) {
+            if (err) {
+                console.log(err);
+            }
 
-			app.post('/access_token', passport.authenticate('oauth', {session: false}), function (req, res) {
-		  		var body 		= req.body;
-				var uid 		= body['user_id'];
-				var fullname 	= body['lis_person_name_full'];
-				var givenname 	= body['lis_person_name_given'];
-				var user_id		= body['user_id'];
-	
-				//Send the access details to the client
-				res.writeHead(200, { 'Content-Type': 'text/html'});
-				res.write('<html><head>');		
-				res.write(core.javascriptToken(user_id));	
-				res.write(html_data);
-				res.write('</body></html>');
-				res.end();
-		 	});
-		});
-	}
-	root.setupPlugin = setupPlugin;
+            app.post('/access_token', passport.authenticate('oauth', {session: false}), function (req, res) {
+                var body        = req.body;
+                var uid         = body['user_id'];
+                var fullname    = body['lis_person_name_full'];
+                var givenname   = body['lis_person_name_given'];
+                var user_id     = body['user_id'];
+    
+                //Send the access details to the client
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.write('<html><head>');        
+                res.write(core.javascriptToken(user_id));    
+                res.write(html_data);
+                res.write('</body></html>');
+                res.end();
+             });
+        });
+    }
+    root.setupPlugin = setupPlugin;
 })();
